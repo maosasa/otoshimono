e5ef8d30dc8c9406cf651ad77ec1b841a71af4b8
Mao Sasakura <huuchitaro@gmail.com>
2021-12-25 21:18:50 +0900

    Add Bootstrap

--- otoshimonoapp/views.py
+++ otoshimonoapp/views.py
@@ -10,3 +10,12 @@ def index(request):
 def detail(request, otoshimono_id):
     otoshimono = get_object_or_404(OtoshimonoInfo, pk=otoshimono_id)
     return render(request, 'otoshimonoapp/detail.html', {'otoshimono': otoshimono})
+
+def about(request):
+    context = {}
+    return render(request, 'otoshimonoapp/about.html', context)
+
+def listing(request):
+    latest_otoshimono_list = OtoshimonoInfo.objects.order_by('-pub_date')[:100]
+    context = {'latest_otoshimono_list': latest_otoshimono_list}
+    return render(request, 'otoshimonoapp/list.html', context)
