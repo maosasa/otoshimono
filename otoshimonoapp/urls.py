efef9e008eba515006404adf564270430b4e5442
Mao Sasakura <huuchitaro@gmail.com>
2021-12-25 17:28:06 +0900

    urlの名前空間を設定

--- otoshimonoapp/urls.py
+++ otoshimonoapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'otoshimonoapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:otoshimono_id>/', views.detail, name='detail'),
