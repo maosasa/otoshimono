929d78c09a12ffa7902f4d10f46ddef9f0386280
Mao Sasakura <huuchitaro@gmail.com>
2021-12-25 16:51:38 +0900

    base

--- otoshimonoapp/models.py
+++ otoshimonoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class OtoshimonoInfo(models.Model):
+    obj_name = models.CharField(max_length=200)
+    place_found = models.CharField(max_length=200, default="")
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.obj_name
--- otoshimonoapp/views.py
+++ otoshimonoapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import OtoshimonoInfo
+
+def index(request):
+    latest_otoshimono_list = OtoshimonoInfo.objects.order_by('-pub_date')[:100]
+    context = {'latest_otoshimono_list': latest_otoshimono_list}
+    return render(request, 'otoshimonoapp/index.html', context)
+
+def detail(request, otoshimono_id):
+    otoshimono = get_object_or_404(OtoshimonoInfo, pk=otoshimono_id)
+    return render(request, 'otoshimonoapp/detail.html', {'otoshimono': otoshimono})
