d80df4ffe3d8ee6de17c356c5f0a62b1742c50ed
Yoshikiyo <mantohihi785@g.ecc.u-tokyo.ac.jp>
2021-12-26 12:05:59 +0900

    mediaのパスを修正

--- otoshimonoapp/models.py
+++ otoshimonoapp/models.py
@@ -5,7 +5,7 @@ class OtoshimonoInfo(models.Model):
     obj_name = models.CharField(max_length=200)
     place_found = models.CharField(max_length=200, default="")
     pub_date = models.DateTimeField('date published')
-    image = models.ImageField(upload_to='media/', default='media/no_image.jpg')
+    image = models.ImageField(upload_to='otoshimono/', default='otoshimono/no_image.jpg')
 
     def __str__(self):
         return self.obj_name
