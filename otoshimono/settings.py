4bb6651057cd7e91a24bb9d1c9d34dadf3a0753f
Yoshikiyo <mantohihi785@g.ecc.u-tokyo.ac.jp>
2021-12-28 00:51:05 +0900

    DEBUG=Falseに変更

--- otoshimono/settings.py
+++ otoshimono/settings.py
@@ -32,9 +32,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '.herokuapp.com',
+]
 
 
 # Application definition
