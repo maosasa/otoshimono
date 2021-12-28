0f56a74d792ebc7c627e4466f4687f56e75053dd
Mao Sasakura <huuchitaro@gmail.com>
2021-12-28 19:51:11 +0900

    Add form page

--- otoshimonoapp/models.py
+++ otoshimonoapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class OtoshimonoInfo(models.Model):
     obj_name = models.CharField(max_length=200)
     place_found = models.CharField(max_length=200, default="")
+    place_now = models.CharField(max_length=200, default="見つけた場所と同じ")
     pub_date = models.DateTimeField('date published')
     image = models.ImageField(upload_to='otoshimono/', default='otoshimono/no_image.jpg')
 
