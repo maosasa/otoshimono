08de0971a9b578f725f438d18214f1eccdf69603
Yoshikiyo <mantohihi785@g.ecc.u-tokyo.ac.jp>
2021-12-28 16:13:28 +0900

    AWS S3を導入

--- otoshimono/settings.py
+++ otoshimono/settings.py
@@ -22,9 +22,6 @@ if not os.environ.get('HEROKU_ENV'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -39,7 +36,6 @@ ALLOWED_HOSTS = [
     '.herokuapp.com',
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'otoshimonoapp.apps.OtoshimonoappConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -144,6 +141,16 @@ MEDIA＿ROOT = os.path.join(BASE_DIR, 'media')
 # 画像をdjango側で読み込むための設定
 MEDIA_URL = '/media/'
 
+if not DEBUG:
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = S3_URL
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
 
 # Activate Django-Heroku.
 if os.environ.get('HEROKU_ENV'):
