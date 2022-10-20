2585983edeb44fe230db842cfca5d3c77226a7f1
Yoshikiyo <mantohihi785@g.ecc.u-tokyo.ac.jp>
2022-10-20 20:15:47 +0900

    forms.pyを導入

---
+++ otoshimonoapp/forms.py
@@ -0,0 +1,34 @@
+from dataclasses import fields
+from pyexpat import model
+from django import forms
+
+from otoshimonoapp.models import OtoshimonoInfo
+
+
+class OtoshimonoRegisterForm(forms.ModelForm):
+    """"落とし物登録画面のフォーム"""
+    limit_image_size = 10 * 1024 * 1024
+
+    class Meta:
+        model = OtoshimonoInfo
+        fields = ['obj_name', 'place_found', 'place_now', 'location', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def clean_obj_name(self):
+        obj_name = self.cleaned_data["obj_name"]
+        if len(obj_name) > 200:
+            raise forms.ValidationError(
+                '200文字以内で入力してください。',
+            )
+        return obj_name
+
+    def clean_image(self):
+        image = self.cleaned_data["image"]
+        if image != None:
+            if image.size > self.limit_image_size:
+                raise forms.ValidationError(
+                    '画像は10MB以下のものをアップロードしてください',
+                )
+        return image
--- otoshimonoapp/views.py
+++ otoshimonoapp/views.py
@@ -4,70 +4,72 @@ import json
 import uuid
 from datetime import datetime, date
 
+from otoshimonoapp.forms import OtoshimonoRegisterForm
+
 from .models import OtoshimonoInfo
 
 # json.dumpでJSON型に変換できない型の例外処理
+
+
 def convertDataToJson(object):
     if isinstance(object, (datetime, date)):
         return object.isoformat()
-    elif(isinstance(object, uuid.UUID)):
+    elif (isinstance(object, uuid.UUID)):
         return str(object)
+
 
 def index(request):
     latest_otoshimono_list = OtoshimonoInfo.objects.order_by('-pub_date')[:100]
     context = {
         'latest_otoshimono_list': json.dumps(list(latest_otoshimono_list.values()), ensure_ascii=False, default=convertDataToJson),
         'image_url_list': json.dumps([item.image.url for item in latest_otoshimono_list], ensure_ascii=False, default=convertDataToJson)
-        }
+    }
     return render(request, 'otoshimonoapp/index.html', context)
+
 
 def detail(request, otoshimono_id):
     otoshimono = get_object_or_404(OtoshimonoInfo, pk=otoshimono_id)
     return render(request, 'otoshimonoapp/detail.html', {'otoshimono': otoshimono})
 
+
 def about(request):
     context = {}
     return render(request, 'otoshimonoapp/about.html', context)
+
 
 def listing(request):
     latest_otoshimono_list = OtoshimonoInfo.objects.order_by('-pub_date')[:100]
     context = {'latest_otoshimono_list': latest_otoshimono_list}
     return render(request, 'otoshimonoapp/list.html', context)
 
+
 def form(request):
     context = {}
     return render(request, 'otoshimonoapp/form.html', context)
+
 
 def form_location(request):
     context = {}
     return render(request, 'otoshimonoapp/form_location.html', context)
 
+
 def add(request):
-    try:
-        title = request.POST['title']
-        place_found = request.POST['place_found']
-        place_now = request.POST['place_now']
-        location = request.POST['location']
-    except (KeyError, OtoshimonoInfo.DoesNotExist):
-        return render(request, 'otoshimonoapp/form.html', {
-            'error_message': "You didn't fill the title.",
-        })
-    else:
-        new_otoshimono = OtoshimonoInfo(
-            obj_name=title,
-            place_found=place_found,
-            place_now=place_now,
-            pub_date=timezone.now(),
-            location=location,
-        )
-        if 'image' in request.FILES:
-            limit_image_size = 10* 1024 * 1024
-            if request.FILES['image'].size > limit_image_size:
-                return render(request, 'otoshimonoapp/form.html', {
-                    'error_message': "Image size must be less than 10MB.",
-                })
-            else:
-                new_otoshimono.image = request.FILES['image']
-        new_otoshimono.save()
-        context = {'success_message': "「"+title+"」 が追加されました。ご報告ありがとうございます。"}
+    form = OtoshimonoRegisterForm(request.POST, request.FILES)
+    is_valid = form.is_valid()
+    if not is_valid:
+        return render(request, 'otoshimonoapp/form.html', {'form': form})
+
+    cleaned_data = form.cleaned_data
+    new_otoshimono = OtoshimonoInfo(
+        obj_name=cleaned_data["obj_name"],
+        place_found=cleaned_data["place_found"],
+        place_now=cleaned_data["place_now"],
+        pub_date=timezone.now(),
+        location=cleaned_data["location"],
+    )
+    if cleaned_data["image"] != None:
+        new_otoshimono.image = cleaned_data["image"]
+    new_otoshimono.save()
+    context = {'success_message': "「" +
+               cleaned_data["obj_name"]+"」 が追加されました。ご報告ありがとうございます。"}
     return render(request, 'otoshimonoapp/form.html', context)
